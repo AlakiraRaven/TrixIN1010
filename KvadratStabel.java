937478aa8844622d4403d103c71f472f57e54a06
AlakiraRaven <66569973+AlakiraRaven@users.noreply.github.com>
2025-05-29 18:02:54 +0200

    Trix 6.02

--- KvadratStabel.java
+++ KvadratStabel.java
@@ -3,11 +3,24 @@
 import org.w3c.dom.Node;
 
 public class KvadratStabel {
-    Node start = null;
-    Node end = null;
+    Node start;
+    Node end;
+    int size = 0;
 
     public boolean erTom() { //returnerer sant hvis, og bare hvis, stabelen er tom.
         return start == null;
+    }
+
+
+    public void leggPaa(Kvadrat k) {
+        if (erTom()) {
+            start = new Node(k);
+        } else {
+            Node next = start;
+
+        }
+
+
     }
 }
 //leggPaa() - et element dyttes på toppen av stabelen.
