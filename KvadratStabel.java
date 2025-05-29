77b89788b779f1587172bc38e18864365f28c0b5
AlakiraRaven <66569973+AlakiraRaven@users.noreply.github.com>
2025-05-29 16:58:10 +0200

    Trix 6.02

--- KvadratStabel.java
+++ KvadratStabel.java
@@ -1,7 +1,14 @@
 //Trix 6.02
+
+import org.w3c.dom.Node;
+
 public class KvadratStabel {
-    
+    Node start = null;
+    Node end = null;
+
+    public boolean erTom() { //returnerer sant hvis, og bare hvis, stabelen er tom.
+        return start == null;
+    }
 }
 //leggPaa() - et element dyttes på toppen av stabelen.
 //taAv() - et element tas av toppen av stabelen og returneres. Hvis stabelen er tom skal den returnere null.
-//erTom() - returnerer sant hvis, og bare hvis, stabelen er tom.
